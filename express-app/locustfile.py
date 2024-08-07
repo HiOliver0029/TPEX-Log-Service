e67544109257e0ede8460cb924e6a0b65ec6f187
HiOliver0029 <hellooliver1717@gmail.com>
2024-08-07 14:03:11 +0800

    add monitor function so that the processor can process several log data at one time

--- express-app/locustfile.py
+++
@@ -1,9 +0,0 @@
-from locust import HttpUser, TaskSet, task
-
-class UserBehavior(TaskSet):
-    @task
-    def get_logs(self):
-        self.client.get("/search?host_name=example")
-
-class WebsiteUser(HttpUser):
-    tasks = [UserBehavior]
